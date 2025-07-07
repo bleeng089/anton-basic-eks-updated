4a418bcdd6cca2d7967c61d7793c0197f5519f24
aaron-dm-mcdonald <mcdonald.dm.aaron@gmail.com>
2025-07-06 17:28:32 -0700

    seed

---
+++ voting-app/seed/generate-votes.sh
@@ -0,0 +1,12 @@
+ # Send votes using apache bench
+VOTE_URL=$VOTE_SERVICE_URL
+echo "Sending 1000 votes for option A..."
+ab -n 1000 -c 50 -p posta -T "application/x-www-form-urlencoded" $VOTE_URL/
+
+echo "Sending 1000 votes for option B..."
+ab -n 1000 -c 50 -p postb -T "application/x-www-form-urlencoded" $VOTE_URL/
+
+echo "Sending 1000 more votes for option A..."
+ab -n 1000 -c 50 -p posta -T "application/x-www-form-urlencoded" $VOTE_URL/
+
+echo "Seeding completed!"
