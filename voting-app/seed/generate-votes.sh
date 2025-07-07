2710fac0a7c4735015983a28381e33587c0e0cbe
aaron-dm-mcdonald <mcdonald.dm.aaron@gmail.com>
2025-07-06 18:15:51 -0700

    add sample yaml

--- voting-app/seed/generate-votes.sh
+++ voting-app/seed/generate-votes.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+# Set default values if environment variables are not provided
+VOTES_A=${VOTES_A:-2000}
+VOTES_B=${VOTES_B:-1000}
+
+echo "Configuration:"
+echo "  Votes for option A: $VOTES_A"
+echo "  Votes for option B: $VOTES_B"
+
 # Wait for vote service to be ready
 echo "Waiting for vote service..."
 until curl -f -s $VOTE_SERVICE_URL > /dev/null 2>&1; do
@@ -9,13 +17,11 @@ done
 
 # Send votes using apache bench
 VOTE_URL=$VOTE_SERVICE_URL
-echo "Sending 1000 votes for option A..."
-ab -n 1000 -c 50 -p posta -T "application/x-www-form-urlencoded" $VOTE_URL/
+echo "Sending $VOTES_A votes for option A..."
+ab -n $VOTES_A -c 50 -p posta -T "application/x-www-form-urlencoded" $VOTE_URL/
 
-echo "Sending 1000 votes for option B..."
-ab -n 1000 -c 50 -p postb -T "application/x-www-form-urlencoded" $VOTE_URL/
-
-echo "Sending 1000 more votes for option A..."
-ab -n 1000 -c 50 -p posta -T "application/x-www-form-urlencoded" $VOTE_URL/
+echo "Sending $VOTES_B votes for option B..."
+ab -n $VOTES_B -c 50 -p postb -T "application/x-www-form-urlencoded" $VOTE_URL/
 
 echo "Seeding completed!"
+echo "Total votes sent: $((VOTES_A + VOTES_B)) ($VOTES_A for A, $VOTES_B for B)"
